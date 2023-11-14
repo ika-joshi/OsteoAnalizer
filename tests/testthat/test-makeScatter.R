486d6535f2ab8462ec977870d529555eb39a2077
Krutika Joshi <krutikadilip.joshi@mail.utoronto.ca>
2023-11-14 04:57:28 -0500

    final edits

--- tests/testthat/test-makeScatter.R
+++ tests/testthat/test-makeScatter.R
@@ -15,7 +15,6 @@ test_that("Check if output is a scatter plot", {
   result <- CleanCSVFile(testData, "fluid_levels","cartilage_thickness","severity_levels","patient_age")
 
   # Correct Parameter
-  plots_created <- makeScatter(result, "fluid_levels","cartilage_thickness")
   scatter_plot <- makeScatter(result, "fluid_levels","cartilage_thickness")
   expect_true(inherits(scatter_plot, "gg"))
 })
