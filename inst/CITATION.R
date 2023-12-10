49056c94e522886dc89d5fae8af8278c33f306f2
Krutika Joshi <krutikadilip.joshi@mail.utoronto.ca>
2023-12-10 15:43:33 -0500

    added citations

---
+++ inst/CITATION.R
@@ -0,0 +1,12 @@
+citHeader("To cite OsteoAnalizer in publications use:")
+
+citEntry(entry = "Unpublished",
+         title        = "OsteoAnalizer: : Analyze data sets related to Osteoarthritis",
+         author       = "Krutika Joshi",
+         note         = "Unpublished",
+         year         = "2023",
+
+         textVersion  =
+           paste("Joshi, K. (2023) OsteoAnalizer: : Analyze data sets related to Osteoarthritis.
+        Unpublished. URL https://github.com/ika-joshi/OsteoAnalizer")
+)
